f214d2cd6b340d91609597959a4787dc63839103
AbirTabib <abir.tabib.497@gmail.com>
2023-01-14 21:05:29 +0100

    first commit

---
+++ TP3_Identity/Models/Repositories/ISchoolRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TP3_Identity.Models.Repositories
+{
+    public interface ISchoolRepository
+    {
+        IList<School> GetAll();
+        School GetById(int id);
+        void Add(School s);
+        void Edit(School s);
+        void Delete(School s);
+        double StudentAgeAverage(int schoolId);
+        int StudentCount(int schoolId);
+    }
+}
